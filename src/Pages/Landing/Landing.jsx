ed014e633760c2c9d27edbaabcdb46b98fb9d8de
IgnacioCava <cavaignacio11@gmail.com>
2022-02-16 00:04:44 -0300

    Completed form components and other stuff

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import background from '../../assets/images/landingBackground.png'
 import './Landing.scss'
+import AuthForm from '../../components/Form/RegisterForm/Form'
+import autoScroll from '../../helpers/autoScroll' // Documentación en el .js
 
 export default function LandingPage(){
 
@@ -9,7 +11,23 @@ export default function LandingPage(){
             <h1 className='title'>Training app</h1>
             <img className='background' src={background} alt='background'/>
             <div className='choices'>
-                <h1>Sign Up</h1><hr/><h1>Log In</h1>
+                <h1 id='signUpText' onClick={()=>{
+                    autoScroll('form', 'left')
+                    document.getElementById('signUpText').style.borderBottom='10px solid #3f59b8'
+                    document.getElementById('logInText').style.borderBottom='10px solid transparent'
+                }}>Sign Up</h1>
+
+                <hr/>
+
+                <h1 id='logInText' onClick={()=>{
+                    autoScroll('form', 'right')
+                    document.getElementById('logInText').style.borderBottom='10px solid #3f59b8'
+                    document.getElementById('signUpText').style.borderBottom='10px solid transparent'
+                }}>Log In</h1>
+            </div>
+            <div id='form'>
+                <AuthForm method={'register'}/>
+                <AuthForm method={'login'}/>
             </div>
             
         </div>
