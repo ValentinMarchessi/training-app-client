f179f2ea76765ac1750d331624b92a66d8547f1f
neriheredia <developer.heredia@gmail.com>
2022-02-16 18:27:52 -0300

    Navbar Complete

--- src/Pages/HomeTrainer/HomeTrainer.jsx
+++ src/Pages/HomeTrainer/HomeTrainer.jsx
@@ -1,55 +1,13 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './homeTrainer.scss'
-import HomeIcon from '@mui/icons-material/Home';
-import MenuIcon from '@mui/icons-material/Menu';
-import SettingsIcon from '@mui/icons-material/Settings';
-import LogoutIcon from '@mui/icons-material/Logout';
-import HistoryIcon from '@mui/icons-material/History';
-import Avatar from '../../assets/images/dep.jpg'
+
 import { listInfo, listNoticias } from '../../data'
+import Navbar from '../../components/Navbar/Navbar'
 
 const HomeTrainer = () => {
-    const [active, setActive] = useState(false)
-
-    const handleClick = () => {
-        setActive(!active)
-    }
-
     return (
         <div className='contentHomeTrainer'>
-            <div className="topBarContainer">
-                <div className="leftItems">
-                    <HomeIcon className="homeItem" />
-                    <span className="page" >Inicio</span>
-                </div>
-                <div className="rightItems">
-                    <div className="contentImg">
-                        <img className="avatarNavbar" src={Avatar} alt="" />
-                    </div>
-                    <div className="contentMenu">
-                        <MenuIcon onClick={handleClick} className="hamburgerItem" />
-                        <div
-                            id="contentSeting"
-                            className={active ? "contentSeting active" : "contentSeting"}>
-                            <div className="triangle" />
-                            <div className="contentList">
-                                <div className="itemList">
-                                    <LogoutIcon className="iconItems" />
-                                    <span className="textSeting" >Log out</span>
-                                </div>
-                                <div className="itemList">
-                                    <SettingsIcon className="iconItems" />
-                                    <span className="textSeting" >Settings</span>
-                                </div>
-                                <div className="itemList">
-                                    <HistoryIcon className="iconItems" />
-                                    <span className="textSeting" >History</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Navbar />
             <div className="downContainer">
                 <div className="leftContain">
                     <div className="contentTitle">
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './navbar.scss'
+import HomeIcon from '@mui/icons-material/Home';
+import MenuIcon from '@mui/icons-material/Menu';
+import SettingsIcon from '@mui/icons-material/Settings';
+import LogoutIcon from '@mui/icons-material/Logout';
+import HistoryIcon from '@mui/icons-material/History';
+import Avatar from '../../assets/images/dep.jpg'
 
 const Navbar = () => {
+    const [active, setActive] = useState(false)
+
+    const handleClick = () => {
+        setActive(!active)
+    }
     return (
         <div className="topBarContainer">
-            <div className="leftItem">ICONO</div>
-            <div className="rightItem">MENU</div>
+            <div className="leftItems">
+                <HomeIcon className="homeItem" />
+                <span className="page" >Inicio</span>
+            </div>
+            <div className="rightItems">
+                <div className="contentImg">
+                    <img className="avatarNavbar" src={Avatar} alt="" />
+                </div>
+                <div className="contentMenu">
+                    <MenuIcon onClick={handleClick} className="hamburgerItem" />
+                    <div
+                        id="contentSeting"
+                        className={active ? "contentSeting active" : "contentSeting"}>
+                        <div className="triangle" />
+                        <div className="contentList">
+                            <div className="itemList">
+                                <LogoutIcon className="iconItems" />
+                                <span className="textSeting" >Log out</span>
+                            </div>
+                            <div className="itemList">
+                                <SettingsIcon className="iconItems" />
+                                <span className="textSeting" >Settings</span>
+                            </div>
+                            <div className="itemList">
+                                <HistoryIcon className="iconItems" />
+                                <span className="textSeting" >History</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
