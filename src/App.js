cc8efbcd7b4985f722a02a3a5c5ce14ba62cbc01
neriheredia <developer.heredia@gmail.com>
2022-02-16 16:54:25 -0300

    Home Trainer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-<<<<<<< HEAD
-import './App.css';
-import Landing from './Pages/Landing/Landing'
-import HomeTrainer from './Pages//HomeTrainer/HomeTrainer'
+
+import "./App.scss";
+// import Landing from "./Pages/Landing/Landing";
+import HomeTrainer from './Pages/HomeTrainer/HomeTrainer'
 
 function App() {
     return (
         <div className="App">
-            {/* <Landing/> */}
             <HomeTrainer />
+            {/* <Landing /> */}
         </div>
     );
-=======
-import "./App.scss";
-import Landing from "./Pages/Landing/Landing";
-
-function App() {
-  return (
-    <div className="App">
-      <Landing />
-    </div>
-  );
->>>>>>> 52711d929e8514f04de655a3d3973ff983fda045
 }
 
 export default App;
