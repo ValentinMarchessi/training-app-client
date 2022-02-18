a9be3736ec69726d8a7669db554c7d75675b7872
IgnacioCava <cavaignacio11@gmail.com>
2022-02-18 14:51:50 -0300

    Finished signup sequence with incomplete animations

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.scss";
 import Landing from "./Pages/Landing/Landing";
 import HomeTrainer from './Pages/HomeTrainer/HomeTrainer'
 import Settings from './Pages/Settings/Settings'
+import SignUpSequence from './Pages/SignUpSequence/SignUpSequence';
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
                 <Route path='/' element={<Landing />} />
                 <Route path='/home' element={<HomeTrainer />} />
                 <Route path='/settings' element={<Settings />} />
+                <Route path='/newUser' element={<SignUpSequence />} />
             </Routes>
         </div>
     );
