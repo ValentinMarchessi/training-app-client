cd8af7f111c8fd7457f916aaceaeb00400abe354
heredia lucas <heredialucasfac22@hotmail.com>
2022-02-21 15:53:23 -0300

    ruteando y configurando settings

--- src/Pages/Settings/Settings.jsx
+++ src/Pages/Settings/Settings.jsx
@@ -3,11 +3,23 @@ import S from "./Settings.module.scss";
 //MUI ICONS
 import SettingsIcon from "@mui/icons-material/Settings";
 import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
-
+//HOOKS
+import { useState } from "react";
+import {useNavigate} from 'react-router-dom'
 //COMPONENTS
 import ContainerUser from "../../components/ContainerUser/ContainerUser";
 
 export default function Settings() {
+  const [config, setConfig] = useState("Account data");
+  const redir = useNavigate()
+
+  function onClick(e) {
+    setConfig(e.target.textContent);
+  }
+  function onRedir() {
+    redir('/')
+  }
+
   return (
     <div className={S.container}>
       <div className={S.containerSideMenu}>
@@ -16,14 +28,15 @@ export default function Settings() {
           <h2>Settings</h2>
         </div>
         <ul className={S.contentSideMenu}>
-          <li>Account data</li>
-          <li>Profile</li>
-          <li>Payment method</li>
+          <li onClick={(e) => onClick(e)}>Account data</li>
+          <li onClick={(e) => onClick(e)}>Profile</li>
+          <li onClick={(e) => onClick(e)}>Payment method</li>
+          <li onClick={(e) => onClick(e)}>History user</li>
         </ul>
       </div>
-      <ContainerUser />
+      <ContainerUser config={config} />
       <div className={S.containerBtn}>
-        <button>
+        <button onClick={onRedir}>
           <CloseRoundedIcon className={S.btnIcon} />
         </button>
         <p>Exit</p>
