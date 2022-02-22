594b7df29bb2af79670822ae65c3adc44315b086
Valentín Marchessi <vmarchessi@gmail.com>
2022-02-22 12:41:25 -0300

    added Pages indexing for cleaner imports

---
+++ src/Pages/index.js
@@ -0,0 +1,23 @@
+import ClientDietDetails from './ClientDietDetails/ClientDietDetails.jsx'
+import MyDiets from './Diets/MyDiets.jsx';
+import Exercises from './Exercises/Exercises.jsx';
+import Home from './Home/Home.jsx';
+import Landing from './Landing/Landing.jsx';
+import Payment from './Payment/Payment.jsx';
+import Routines from './Routines/Routines.jsx';
+import Search from './Search/Search.jsx';
+import Settings from './Settings/Settings.jsx';
+import SignUpSequence from './SignUpSequence/SignUpSequence.jsx';
+
+export {
+    ClientDietDetails,
+    MyDiets,
+    Exercises,
+    Home,
+    Landing,
+    Payment,
+    Routines,
+    Search,
+    Settings,
+    SignUpSequence,
+}
