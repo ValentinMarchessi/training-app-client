6e3d343f9eaf1322a8cfafb71b881834a727d197
Valentín Marchessi <vmarchessi@gmail.com>
2022-02-22 18:36:39 -0300

    added and refactored exercises page

--- src/Pages/Exercises/ExercisesCreate/ExercisesCreate.jsx
+++ src/Pages/Exercises/ExercisesCreate/ExercisesCreate.jsx
@@ -1,9 +1,27 @@
 import style from './ExercisesCreate.module.scss';
+import ExerciseForm from '../ExerciseForm/ExerciseForm';
+import { Overlay } from '../../../components';
+import { useState } from 'react';
 
 export default function ExercisesCreate() {
+    const [overlay, setOverlay] = useState(false)
+    const overlayStyle = {
+        backgroundColor: '#201F24',
+	};
+    
+    const handleOverlay = {
+        open: () => setOverlay(true),
+        close: () => setOverlay(false),
+    }
+
     return (
-        <div className={style.container}>
-            
-        </div>
-    )
+		<>
+			<Overlay active={overlay} onClose={handleOverlay.close} style={overlayStyle}>
+				<ExerciseForm onAdd={handleOverlay.close} onClose={handleOverlay.close} />
+			</Overlay>
+			<button id={style.add} onClick={handleOverlay.open}>
+				Add Exercise
+			</button>
+		</>
+	);
 }
