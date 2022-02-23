8429291247b95fdc4600ea4166a06879d476ae6b
unknown <apoc88@protonmail.com>
2022-02-23 15:18:20 -0500

    base for routine creation form

---
+++ src/Pages/Routines/RoutineForm/RoutineForm.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { CreateRoutines } from '../../../Redux/apiCalls/rutinesCall/createRoutines.js';
+
+const RoutineForm = () => {
+      const [data, setData] = useState({
+            owner: currentUser.id,
+            title: '',
+            price: 0,
+            exercises: [] // need to fetch from state / props
+      });
+      const currentUser = useSelector(state => state.currentUser);
+      const dispatch = useDispatch();
+
+      const handleChange = (e) => {
+            const property = e.target.id;
+            const value = e.target.value;
+
+            setData({ ...data, [property]: value });
+      };
+
+      const handleSubmit = (e) => {
+            const error = Object.keys(data).map(key => (!data[key] || !data[key].length) && key).filter(e => e)[0];
+            if (error) {
+                  alert(`Error! field '${error}' can't be empty.`);
+                  e.preventDefault();
+            } else {
+                  e.preventDefault();
+                  CreateRoutines(dispatch, data, currentUser.accessToken);
+                  alert('Routine Created');
+            }
+      };
+
+      return <div>
+            <form action="" onChange={(e) => handleChange(e)}>
+                  <label>Title</label>
+                  <input type='text' id='title' />
+                  <label>price</label>
+                  <input type='number' id='price' />
+                  <label>Exercises</label>
+
+                  <input type='submit' value='Create recipe' onClick={(e) => handleSubmit(e)} />
+            </form>
+      </div>;
+};
+
+export default RoutineForm;
