1afaedde10a991606c962884042162ae723efb62
Valentín Marchessi <vmarchessi@gmail.com>
2022-02-23 14:36:49 -0300

    home handling methods

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,17 +1,20 @@
 import React, { useEffect } from 'react';
-import './home.scss';
+import styles from './Home.module.scss';
 import Navbar from '../../components/Navbar/Navbar';
-import { useDispatch, useSelector } from 'react-redux'
-import { Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux'
+import HomeProfessional from './HomeProfessional/HomeProfessional';
+import HomeClient from './HomeClient/HomeClient';
 
 const Home = () => {
     const user = useSelector(state => state.user.currentUser)
+    console.log(user)
+    /* La store no está guardando isTrainer ni isNutritionist, hay que revisar */
     return (
-        <div className="contentHomeTrainer">
-            <Navbar/>
-            <Outlet></Outlet>
-        </div>
-    );
+		<div className={styles.page}>
+			<Navbar />
+			{user && (user.isTrainer || user.isNutritionist) ? <HomeProfessional/> : <HomeClient/>}
+		</div>
+	);
 };
 
 export default Home;
