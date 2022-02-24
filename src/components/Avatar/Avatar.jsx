929a7f60301828008c6e96373553afb97933254e
Valentín Marchessi <vmarchessi@gmail.com>
2022-02-24 02:16:16 -0300

    avatar placeholder

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -2,5 +2,9 @@ import styles from './Avatar.module.scss';
 import placeholder from '../../assets/images/avatarPlaceholder.svg';
 
 export default function Avatar({ src, style }) {
-    return <img style={style} className={styles.avatar} alt="avatar" src={src || placeholder} />;
+    return (
+		<div id={styles.placeholder}>
+			<img style={style} className={styles.avatar} src={src || placeholder} />
+		</div>
+	);
 }
