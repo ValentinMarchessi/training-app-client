99a31f368930bfb5e24c4b568c9f6952f2d79364
unknown <apoc88@protonmail.com>
2022-02-25 17:00:07 -0500

    up to date with dev

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import placeholder from '../../assets/images/avatarPlaceholder.svg';
 import styles from './Avatar.module.scss';
 
 export default function Avatar({ src, style }) {
 	return (
 		<div id={styles.thumb}>
-			<img style={style} src={src} />
+			<img style={style} src={src || placeholder} />
 		</div>
 	);
 }
