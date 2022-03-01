a92034eef6c802824b952275a2edfcbb7a235c79
Valentín Marchessi <vmarchessi@gmail.com>
2022-03-01 12:06:53 -0300

    endpoint fix

--- src/Redux/apiCalls/getNewsCall/getNewsCall.js
+++ src/Redux/apiCalls/getNewsCall/getNewsCall.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 import {errorNews, successNews, startGetNews} from '../../reducers/newsReducer';
+import { baseUrlDev } from '../../../config/requestMethod/publicRequest';
 
 export const getNews = async ( dispatch )=>{
-      const url = 'http://localhost:8200/api/news';
       dispatch(startGetNews());
       try {
-            const result = await axios.get(url);
+            const result = await baseUrlDev.get('/news');
             console.log('Esto tiene result', result.data);
             dispatch( successNews(result.data));
       } catch (error) {
