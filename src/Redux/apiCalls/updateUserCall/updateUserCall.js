139c09790f738898165a40f31fb396d56dd94b85
Valentín Marchessi <vmarchessi@gmail.com>
2022-03-01 13:16:10 -0300

    update user fix

--- src/Redux/apiCalls/updateUserCall/updateUserCall.js
+++ src/Redux/apiCalls/updateUserCall/updateUserCall.js
@@ -1,24 +1,24 @@
 import { baseUrlDev } from '../../../config/requestMethod/publicRequest';
-import {
-  updateFailure, updateStart,
-  updateSuccess
-} from '../../reducers/userUpdateReducer';
+import { updateFailure, updateStart, updateSuccess } from '../../reducers/userUpdateReducer';
 
 //UPDATE USER
-export const updateUser = async (dispatch, data) => {
-  dispatch(updateStart());
-  try {
-    const res = await baseUrlDev.put("user/update", {
-      params: {
-        userId: data.userId
-      },
-      data: {
-        field: data.field,
-        value: data.value
-      }
-    });
-    dispatch(updateSuccess(res.data));
-  } catch (err) {
-    dispatch(updateFailure());
-  }
+export const updateUser = async (dispatch, accessToken, userId, data) => {
+	dispatch(updateStart());
+	try {
+		const res = await baseUrlDev.put(
+			`update/${userId}`,
+			{
+				username: data.username,
+				password: data.password,
+				email: data.email,
+				profile_img: data.profile_img,
+			},
+			{
+				headers: { token: accessToken },
+			}
+		);
+		dispatch(updateSuccess(res.data));
+	} catch (err) {
+		dispatch(updateFailure());
+	}
 };
