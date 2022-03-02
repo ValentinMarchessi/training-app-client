d4fd2d6dfdd9a6a12db5f0c5be637a1105860c47
Valentín Marchessi <vmarchessi@gmail.com>
2022-03-01 22:26:37 -0300

    fixed imports

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import "./App.scss";
 import { Clients, Diets, Exercises, Home, Landing, Payment, Routines, Search, Settings, SignUpSequence, Recipes } from './Pages';
 import { ExercisesCreate, ExercisesView } from './Pages/Exercises';
 import { AccountUser, HistoryUser, PaymentMethod, ProfileUser } from './Pages/Settings';
-import CreateDiet from './components/CreateDiet/CreateDiet';
-import CreateRecipe from './components/CreateRecipe/CreateRecipe';
-import RoutineDetail from './Pages/Routines/RoutineDetail/RoutineDetail';
+import { CreateDiet } from './Pages/Diets';
+import { CreateRecipe } from './Pages/Recipes';
+import { RoutineDetail } from './Pages/Routines';
 
 function App() {
     return (
