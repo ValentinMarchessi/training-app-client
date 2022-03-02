a52b853954dfdbe4fbff57313c89341db99750b9
Valentín Marchessi <vmarchessi@gmail.com>
2022-03-02 13:35:45 -0300

    backend url

--- src/config/requestMethod/publicRequest.js
+++ src/config/requestMethod/publicRequest.js
@@ -1,4 +1,4 @@
 import axios from 'axios'
 export const baseUrlDev = axios.create({
-	baseURL: process.env.REACT_APP_BACKEND_URL || 'https://back-app-training.herokuapp.com/api'
+	baseURL: process.env.REACT_APP_BACKEND_URL || 'https://training-app-back-end.herokuapp.com/api',
 });
