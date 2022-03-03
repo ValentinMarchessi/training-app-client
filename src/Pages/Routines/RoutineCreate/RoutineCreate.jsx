638b900410819469787d0c1b9b9f553c2c16d321
unknown <apoc88@protonmail.com>
2022-03-03 11:40:49 -0500

    fixed styles

---
+++ src/Pages/Routines/RoutineCreate/RoutineCreate.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { Overlay } from '../../../components';
+import { RoutineForm } from '../../index.js';
+import style from './RoutineCreate.module.scss';
+
+
+export default function RoutineCreate() {
+  const [active, setActive] = useState(false);
+  const overlayStyle = {
+    backgroundColor: '#201f24c3',
+    width: 'max-content'
+  };
+
+  const handleOverlay = {
+    open: () => setActive(true),
+    close: () => setActive(false),
+  };
+
+  console.log(active);
+
+  return (
+    <>
+      <Overlay active={active} onClose={handleOverlay.close} style={overlayStyle}>
+        <RoutineForm onAdd={handleOverlay.close} />
+      </Overlay>
+      <button id={style.add} onClick={handleOverlay.open}>
+        Create Routine
+      </button>
+    </>
+  );
+}
