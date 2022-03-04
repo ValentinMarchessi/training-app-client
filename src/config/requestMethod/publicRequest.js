8edba4979385116a3ef352b13cb0b53dc529117a
Valentín Marchessi <vmarchessi@gmail.com>
2022-03-04 17:56:36 -0300

    added conditional env backend url (needs testing)

--- src/config/requestMethod/publicRequest.js
+++ src/config/requestMethod/publicRequest.js
@@ -1,4 +1,6 @@
 import axios from 'axios'
+const { REACT_APP_DEV_BACKEND_URL, REACT_APP_PROD_BACKEND_URL, NODE_ENV } = process.env;
+const isProd = NODE_ENV === 'production';
 export const baseUrlDev = axios.create({
-    baseURL: process.env.REACT_APP_BACKEND_URL
-})
+	baseURL: isProd ? REACT_APP_PROD_BACKEND_URL : REACT_APP_DEV_BACKEND_URL,
+});
