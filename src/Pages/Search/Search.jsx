a2340ae76d7a2f1c0042d4f4c577cf0054aefc7d
Valentín Marchessi <vmarchessi@gmail.com>
2022-03-08 13:05:22 -0300

    fallbacks

--- src/Pages/Search/Search.jsx
+++ src/Pages/Search/Search.jsx
@@ -8,8 +8,7 @@ import { getAllTrainers } from '../../Redux/apiCalls/allUsersTrainer/allUsersTra
 import { getAllNutritionits } from '../../Redux/apiCalls/allUsersNutritionist/allUsersNutritionist';
 import { getAllRoutines } from '../../Redux/apiCalls/rutinesCall/getAllRoutines';
 import { getAllDiets } from '../../Redux/apiCalls/dietsCall/getAllDiets';
-import { Navbar } from '../../components';
-import { Select } from '../../components';
+import { Navbar, Select, Fallback } from '../../components';
 
 import RoutineCard from './components/RoutineCard/RoutineCard';
 import UserCard from './components/UserCard/UserCard';
@@ -91,78 +90,91 @@ export default function Search() {
     };
 
     return (
-        <>
-            <Navbar />
-            <div className={s.page}>
-                <div className={s.search}>
-                    <div className={s.fields}>
-                        {/*Esto se debe mejorar : Son los filtros */}
-                        <button onClick={() => sortHandler('reviews')}>
-                            Reviews
-                        </button>
-                        <button onClick={() => sortHandler('rating')}>
-                            Raiting
-                        </button>
-                        <button onClick={() => sortHandler('price')}>
-                            Price
-                        </button>
-                    </div>
-                    <Select callback={(e) => setCurrentType(e.target.value)} options={[{ value: 'Routines' }, { value: 'Diets' }, { value: 'Nutritionists' }, { value: 'Personal Trainers' }]} />
-                    {/* Esta es el input de busqueda */}
-                    <Searchinput callback={setCurrentItems} setInput={newInput} type={currentType} />
-                </div>
-                <h2 id={s.results}>{search && `Resultados para: ${search}`}</h2>
-
-                <div id='paginationContainer' className={s.resultContainer}>
-                    {/* Aqui es donde se muestra todo */}
-                    {currentType === 'Nutritionists' || currentType.includes('Trainers') ? current.map(element =>
-                        <Link to={`/userDetail/${element.id}`} style={{ textDecoration: 'none', color: 'unset', margin: '10px' }}>
-                            <UserCard
-                                id={element.id}
-                                profileImg={element.profile_img}
-                                username={element.username}
-                                email={element.email}
-                                gender={element.gender}
-                                country={element.country}
-                                nutritionist={element.is_nutritionist}
-                                PTrainer={element.is_personal_trainer}
-                            />
-                        </Link>
-                    )
-                        : current.map(element =>
-                            currentType === 'Routines' ? <Link to={`/routineDetail/${element.id}`} style={{ textDecoration: 'none', color: 'unset', margin: '10px' }}>
-                                <RoutineCard
-                                    avatar={element.owner?.profile_img}
-                                    author={element.owner?.username}
-                                    email={element.owner?.email}
-                                    title={element.title}
-                                    rating={element.rating}
-                                    reviews={element.reviews}
-                                    price={element.price}
-                                    image={element.image}
-                                />
-                            </Link>
-                                : <Link to={`/dietDetail/${element.id}`} style={{ textDecoration: 'none', color: 'unset', margin: '10px' }}>
-                                    <RoutineCard
-                                        avatar={element.owner?.profile_img}
-                                        author={element.owner?.username}
-                                        email={element.owner?.email}
-                                        title={element.title}
-                                        rating={element.rating}
-                                        reviews={element.reviews}
-                                        price={element.price}
-                                        image={element.image}
-                                    />
-                                </Link>
-                        )}
-                </div>
-
-                <div className={s.pagination}>
-                    <button onClick={() => inicio !== 0 ? setInicio(inicio - 8) : null}>Previous</button>
-                    <button onClick={() => inicio + 8 < currentItems.length ? setInicio(inicio + 9) : null}>Next</button>
-                </div>
-
-            </div>
-        </>
-    );
+		<>
+			<Navbar />
+			<div className={s.page}>
+				<div className={s.search}>
+					<div className={s.fields}>
+						{/*Esto se debe mejorar : Son los filtros */}
+						<button onClick={() => sortHandler('reviews')}>Reviews</button>
+						<button onClick={() => sortHandler('rating')}>Raiting</button>
+						<button onClick={() => sortHandler('price')}>Price</button>
+					</div>
+					<Select
+						callback={(e) => setCurrentType(e.target.value)}
+						options={[
+							{ value: 'Routines' },
+							{ value: 'Diets' },
+							{ value: 'Nutritionists' },
+							{ value: 'Personal Trainers' },
+						]}
+					/>
+					{/* Esta es el input de busqueda */}
+					<Searchinput callback={setCurrentItems} setInput={newInput} type={currentType} />
+				</div>
+				<h2 id={s.results}>{search && `Resultados para: ${search}`}</h2>
+				<Fallback>
+					<div id="paginationContainer" className={s.resultContainer}>
+						{/* Aqui es donde se muestra todo */}
+						{currentType === 'Nutritionists' || currentType.includes('Trainers')
+							? current.map((element) => (
+									<Link
+										to={`/userDetail/${element.id}`}
+										style={{ textDecoration: 'none', color: 'unset', margin: '10px' }}>
+										<UserCard
+											id={element.id}
+											profileImg={element.profile_img}
+											username={element.username}
+											email={element.email}
+											gender={element.gender}
+											country={element.country}
+											nutritionist={element.is_nutritionist}
+											PTrainer={element.is_personal_trainer}
+										/>
+									</Link>
+							  ))
+							: current.map((element) =>
+									currentType === 'Routines' ? (
+										<Link
+											to={`/routineDetail/${element.id}`}
+											style={{ textDecoration: 'none', color: 'unset', margin: '10px' }}>
+											<RoutineCard
+												avatar={element.owner?.profile_img}
+												author={element.owner?.username}
+												email={element.owner?.email}
+												title={element.title}
+												rating={element.rating}
+												reviews={element.reviews}
+												price={element.price}
+												image={element.image}
+											/>
+										</Link>
+									) : (
+										<Link
+											to={`/dietDetail/${element.id}`}
+											style={{ textDecoration: 'none', color: 'unset', margin: '10px' }}>
+											<RoutineCard
+												avatar={element.owner?.profile_img}
+												author={element.owner?.username}
+												email={element.owner?.email}
+												title={element.title}
+												rating={element.rating}
+												reviews={element.reviews}
+												price={element.price}
+												image={element.image}
+											/>
+										</Link>
+									)
+							  )}
+					</div>
+				</Fallback>
+				<div className={s.pagination}>
+					<button onClick={() => (inicio !== 0 ? setInicio(inicio - 8) : null)}>Previous</button>
+					<button onClick={() => (inicio + 8 < currentItems.length ? setInicio(inicio + 9) : null)}>
+						Next
+					</button>
+				</div>
+			</div>
+		</>
+	);
 }
