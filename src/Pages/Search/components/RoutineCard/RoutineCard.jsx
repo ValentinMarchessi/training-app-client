0846054e7d0af8c9be88355d0aa6f10127896d6e
BrunoAmerio <88524640+BrunoAmerio@users.noreply.github.com>
2022-03-08 15:59:52 -0300

    CResponsive, lógica y deiseños agregados y corregidos

--- src/Pages/Search/components/RoutineCard/RoutineCard.jsx
+++ src/Pages/Search/components/RoutineCard/RoutineCard.jsx
@@ -4,28 +4,29 @@ import { star } from '../../../../assets/images/icons';
 
 export default function RoutineCard( {name, author, email, title, rating, reviews, price, image, avatar} ) {
 /* De ser necesaria una refactorización, se puede extraer un componente SmallProfile del div con className={style.profile}*/
+console.log('Raiting', rating)
 return (
 	<div className={style.card}>
 		<div className={style.profile}>
-		<img id={style.avatar} alt={author} src={avatar ? avatar : avatarPlaceholder} />
-		<div className={style.info}>
-			<h1 style={{color:'white'}}>{author}</h1>
-			<h2 style={{color:'white'}}>{email}</h2>
+			<img id={style.avatar} alt={author} src={avatarPlaceholder}/>
+			<div className={style.info}>
+				<h1 style={{color:'white'}}>{author}</h1>
+				<h2 style={{color:'white'}}>{email}</h2>
+			</div>
 		</div>
-		</div>
+
 		<div className={style.body}>
-			<img id={style.background} alt={name} src={image}></img>
-			<h1>{title}</h1>
+			<img alt={name} src={image}></img>
 		</div>
+
 		<div className={style.footer}>
-			<div id={style.merit}>
-			<p id={style.rating}><img src={star} alt="star"/>{rating}/5</p>
-				<p className={style.subtle}>({reviews} reseñas)</p>
-		</div>
-		<div id={style.pricing}>
-			<p className={style.subtle}>A partir de</p>
-			<strong>${price} por clase</strong>
-		</div>
+			<h1>{title}</h1>
+			<strong>Precio : ${price}</strong>
+			
+			<div className={style.merit}>
+				<p className={style.rating}> <img src={star} alt="star"/> {rating ? rating : 0}/5 </p>
+				<p className={style.subtle}> ({reviews} reseñas)</p>
+			</div>
 		</div>
 	</div>
 	);
