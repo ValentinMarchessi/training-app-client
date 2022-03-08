d1f579c1806d3b3c2da941c4a511c4de5918efa6
Valentín Marchessi <vmarchessi@gmail.com>
2022-03-08 15:46:17 -0300

    reformatted userCard

--- src/Pages/Search/components/UserCard/UserCard.jsx
+++ src/Pages/Search/components/UserCard/UserCard.jsx
@@ -1,18 +1,11 @@
 import React from "react";
+import { Avatar } from "../../../../components";
 import s from './UserCard.module.scss';
 
-const UserCard = ( {id, email, profileImg, username,gender,country,nutritionist,PTrainer} )=>{
-      return <div key={id} className={s.container}>
-            <img src={profileImg} alt={username} className={s.profileImg}/>
-            <div className={s.info}>
-                  <h1 className={s.title}>Username : {username}</h1>
-                  <h1 className={s.title}>Email : {email}</h1>
-                  <h1 className={s.title}>Gender : {gender}</h1>
-                  <h1 className={s.title}>Country : {country}</h1>
-                  <h1 className={s.title}>Personal Trainer : {nutritionist ? 'Si' : 'No'}</h1>
-                  <h1 className={s.title}>Nutritionist : {PTrainer ? 'Si' : 'No'}</h1>
-                  
-            </div>
+const UserCard = ( {profileImg, username} )=>{
+      return <div className={s.container}>
+            <Avatar src={profileImg} /> 
+            <h1>{username}</h1>
       </div>
 }
 export default UserCard;
