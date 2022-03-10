3e9b8fb601d76a5cbe7f5a515913391fe2fb17fd
neriheredia <developer.heredia@gmail.com>
2022-03-10 10:54:05 -0300

    test

--- src/Redux/apiCalls/rutinesCall/getRoutinesById.js
+++ src/Redux/apiCalls/rutinesCall/getRoutinesById.js
@@ -1,18 +1,21 @@
 import { baseUrlDev } from "../../../config/requestMethod/publicRequest";
 import {
-  getRoutinesByIdFailure, getRoutinesByIdStart,
-  getRoutinesByIdSuccess
+    getRoutinesByIdFailure, getRoutinesByIdStart,
+    getRoutinesByIdSuccess
 } from "../../reducers/routinesReducer.js";
-  
+
 //get routines By Id
-  
+
 export const getRoutinesById = async (dispatch, id, token) => {
-  dispatch(getRoutinesByIdStart());
-  try {
-    const res = await baseUrlDev.get(`routine/get/${id}`,{headers: {token}});
-    dispatch(getRoutinesByIdSuccess(res.data));
-  } catch (err) {
-    dispatch(getRoutinesByIdFailure());
-  }
+    dispatch(getRoutinesByIdStart());
+    try {
+        const res = await baseUrlDev.get(`/routine/get/${id}`, {
+            headers: {
+                token
+            }
+        });
+        dispatch(getRoutinesByIdSuccess(res.data));
+    } catch (err) {
+        dispatch(getRoutinesByIdFailure());
+    }
 };
-  
