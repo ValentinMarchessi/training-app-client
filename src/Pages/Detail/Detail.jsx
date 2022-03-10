8e5ee0cc5850ad5a9da5e717527270b104995aa2
neriheredia <developer.heredia@gmail.com>
2022-03-10 12:36:31 -0300

    pament

--- src/Pages/Detail/Detail.jsx
+++ src/Pages/Detail/Detail.jsx
@@ -37,87 +37,6 @@ export default function Details(props) {
         })();
     }, []);
 
-<<<<<<< HEAD
-            <div className={styles.presentation}>
-              <Avatar src={owner[0].profile_img} />
-
-              <div className={styles.info}>
-                <p id={styles.name}>{owner[0].username}</p>
-
-                <p id={styles.title}>
-                  {" "}
-                  {owner[0].is_personal_trainer
-                    ? owner[0].is_nutritionist
-                      ? "Personal trainer / Nutricionist"
-                      : "Personal trainer"
-                    : "Nutricionist"}
-                </p>
-              </div>
-              <div id={styles.merit}>
-                <img src={star} alt="star" />
-                <p id={styles.rating}>{rating}/5 </p>
-                <p id={styles.reviews}>({points.length} reseñas)</p>
-              </div>
-            </div>
-          </div>
-
-          <img
-            id={styles.routineImage}
-            src={myProduct.image}
-            alt="routineImage"
-          />
-        </div>
-
-        <div className={styles.checkout}>
-          <h1>{myProduct.title}</h1>
-          <div id={styles.merit}>
-            <img src={star} alt="star" />
-            <p id={styles.rating}>{rating}/5 </p>
-            <p id={styles.reviews}>({points.length} reseñas)</p>
-          </div>
-          <br />
-          <p id={styles.description}>{myProduct.description}</p>
-          <br />
-
-          <p id={styles.title2}>
-            {" "}
-            {owner[0].is_personal_trainer
-              ? owner[0].is_nutritionist
-                ? "Personal trainer / Nutricionist"
-                : "Personal trainer"
-              : "Nutricionist"}
-          </p>
-          <div className={styles.alignCheck}>
-            <Avatar src={owner[0].profile_img} />
-            <div className={styles.info}>
-              <p id={styles.name}>{owner[0].username}</p>
-              <p id={styles.title}>
-                {" "}
-                {owner[0].is_personal_trainer
-                  ? owner[0].is_nutritionist
-                    ? "Personal trainer / Nutricionist"
-                    : "Personal trainer"
-                  : "Nutricionist"}
-              </p>
-              <Link to={"/instructor/" + owner[0].id}>
-                <button id={styles.profileButton}>Ver perfil</button>
-              </Link>
-            </div>
-          </div>
-
-          <div id={styles.checkoutButton}>
-            <Link to="/payment">
-              {/* {navigate("/payment", { state: { myProduct } })} */}
-              <button>Comprar producto ${myProduct.price}</button>
-            </Link>
-          </div>
-        </div>
-      </div>
-    </>
-  ) : (
-    <p>Loading...</p>
-  );
-=======
     if (myDiet.id === id) {
         useEffect(() => {
             setOwner(allProfessionals.filter((e) => e.id === myDiet.owner));
@@ -232,5 +151,4 @@ export default function Details(props) {
     ) : (
         <p>Loading...</p>
     );
->>>>>>> dietsReparar
 }
--- src/Redux/apiCalls/transaction/getTransactions.js
+++ src/Redux/apiCalls/transaction/getTransactions.js
@@ -6,11 +6,11 @@ import {
 
 import { baseUrlDev } from '../../../config/requestMethod/publicRequest';
 
-export const getTransactions = async (dispatch, token, userId) => {
+export const getTransactions = async (dispatch, userId) => {
     dispatch(getTransactionsStart());
     try {
-        const res = await baseUrlDev.get(`/transaction/history/${userId}`, { headers: { token } });
-        dispatch(getTransactionsSuccess(res.data));   
+        const res = await baseUrlDev.get(`transaction/history/${userId}`);
+        dispatch(getTransactionsSuccess(res.data));
     }
     catch (error) {
         dispatch(getTransactionsError());
